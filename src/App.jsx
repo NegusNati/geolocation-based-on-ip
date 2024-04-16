dc88568d361b04ae35f7bbe78648ac6319897574
Negus <natnaelbirhanu22@gmail.com>
2024-04-16 15:43:55 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,27 +16,6 @@ function App() {
 
   }
 
-  // function handelClick() {
-  //   setCountClicks(countClicks + 1);
-
-  //   if (navigator.geolocation) {
-  //     setIsLoading(true);
-  //     navigator.geolocation.getCurrentPosition((position) => {
-  //       setPosition({
-  //         latitude: position.coords.latitude,
-  //         longitude: position.coords.longitude,
-  //       });
-  //     }, (error) => {
-  //       setError(error.message) 
-  //     });
-  //   }else {
-  //     setError("Geolocation is not supported by this browser.")
-  //     alert("Geolocation is not supported by this browser.")
-  //   }
-  //   setIsLoading(false);
-
-  // }
-
   return (
     <div>
       <button onClick={handelClick} disabled={isLoading} >Get my position </button>
@@ -45,7 +24,7 @@ function App() {
       {lat && (
         <p>
           Your GPS position is: 
-         <a target="_blank" rel="noreferrer" href={`https://www.google.com/maps/@${lat},${long},15z`}> Lat: {lat} Long: {long}</a>
+         <a target="_blank" rel="noreferrer" href={`https://www.google.com/maps/@${lat},${long},18z`}> Lat: {lat} Long: {long}</a>
         </p>
       )}
       <p>You requested position {countClicks} times</p>
